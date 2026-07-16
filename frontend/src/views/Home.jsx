1c90f454dde58a5a5b792507ff567e452d9f9b97
Zhihan Liu <2LIUZ98@solent.ac.uk>
2026-07-16 12:17:57 +0100

    FRONTEND

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -5,7 +5,7 @@ import {
   Heart,
   Users,
 } from "lucide-react";
-import Header from "../components/Header";
+import Header from "../components/header";
 import Footer from "../components/footer";
 
 export default function Home() {
